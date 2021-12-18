04a7f78858919da08838e9cee5ee55efe8134ecf
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-12-18 12:53:02 +0100

    Remove 64bit url from onefetch

--- onefetch/update.ps1
+++ onefetch/update.ps1
@@ -5,10 +5,8 @@ $releases = 'https://github.com/o2sh/onefetch/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -17,15 +15,14 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $url = $download_page.links | 
-        Where-Object href -match "onefetch-win.tar.gz" | 
-        Select-Object -First 1 -expand href
-     $url = 'https://github.com' + $url
+    Where-Object href -match "onefetch-win.tar.gz" | 
+    Select-Object -First 1 -expand href
+    $url = 'https://github.com' + $url
 
     $version = $url -split '/' | Select-Object -Last 1 -Skip 1
 
     return @{
         URL32   = $url
-        URL64   = $url
         Version = $version.Replace('v', '')
     }
 }
