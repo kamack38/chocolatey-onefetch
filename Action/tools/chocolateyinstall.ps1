29a18c4cc32954038fc12a248cb78dc0af166811
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-01-24 16:55:11 +0100

    refactor: 💡 Revert back to silent params

--- Action/tools/chocolateyinstall.ps1
+++ Action/tools/chocolateyinstall.ps1
@@ -1,8 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkFile = Join-Path $toolsDir "chocolateyInstall.ahk"
 
 $packageArgs = @{
   packageName    = $packageName
@@ -10,13 +8,8 @@ $packageArgs = @{
   url            = 'https://downloads.mirillis.com/files/action_4_24_3_setup.exe'
   checksum       = 'aacefa46cd44e43dc77668122d78a76cce3dbd5b9aee45705e7b886b9fa4b2bc'
   checksumType   = 'sha256'
+  silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
-Write-Output "Running Autohotkey installer"
-
-Start-Process -FilePath 'AutoHotkey' `
-  -ArgumentList $ahkFile `
-  -PassThru
-Write-Output "Running Action installer"
 Install-ChocolateyPackage @packageArgs
