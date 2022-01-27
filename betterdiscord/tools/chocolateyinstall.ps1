9fd64e1f9e2584ee13d1f1689ac6eafd7bd28185
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-01-27 09:59:30 +0100

    feat: 🎸 Add uninstaller for BetterDiscord

--- betterdiscord/tools/chocolateyinstall.ps1
+++ betterdiscord/tools/chocolateyinstall.ps1
@@ -3,20 +3,12 @@
 $packageName = $env:chocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkFile = Join-Path $toolsDir "chocolateyInstall.ahk"
+$installLocation = Join-Path $toolsDir "BetterDiscord.exe"
 
-$packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'exe'
-  url            = 'https://github.com/BetterDiscord/Installer/releases/download/v1.1.1/BetterDiscord-Windows.exe'
-  checksum       = '50ed26767c4866ebd94f76d23cb1c535df16d3f87840fb3dba3fd77409f00d0b'
-  checksumType   = 'sha256'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
-}
-
-Write-Output "Running Autohotkey installer"
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $installLocation -Url 'https://github.com/BetterDiscord/Installer/releases/download/v1.1.1/BetterDiscord-Windows.exe' -ChecksumType 'sha256' -Checksum '50ed26767c4866ebd94f76d23cb1c535df16d3f87840fb3dba3fd77409f00d0b'
+Write-Output "Running BetterDiscord installer"
+Start-Process -FilePath $installLocation -PassThru
+Write-Output "Running Autohotkey script"
 Start-Process -FilePath 'AutoHotkey' `
   -ArgumentList $ahkFile `
-  -PassThru
-Write-Output "Running Action installer"
-Install-ChocolateyPackage @packageArgs
+  -Wait
