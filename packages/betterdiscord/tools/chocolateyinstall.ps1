6c3505d404f12732ed5d8cb3ad3f3b24480841c9
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-01-28 10:45:51 +0100

    fix: 🐛 Fix Jarfix
    Fix Jarfix & clean BetterDiscord installer

--- packages/betterdiscord/tools/chocolateyinstall.ps1
+++ packages/betterdiscord/tools/chocolateyinstall.ps1
@@ -5,7 +5,15 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkFile = Join-Path $toolsDir "chocolateyInstall.ahk"
 $installLocation = Join-Path $toolsDir "BetterDiscord.exe"
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $installLocation -Url 'https://github.com/BetterDiscord/Installer/releases/download/v1.1.1/BetterDiscord-Windows.exe' -ChecksumType 'sha256' -Checksum '50ed26767c4866ebd94f76d23cb1c535df16d3f87840fb3dba3fd77409f00d0b'
+$packageArgs = @{
+  packageName  = $packageName
+  fileFullPath = $installLocation
+  url          = 'https://github.com/BetterDiscord/Installer/releases/download/v1.1.1/BetterDiscord-Windows.exe'
+  checksum     = '50ed26767c4866ebd94f76d23cb1c535df16d3f87840fb3dba3fd77409f00d0b'
+  checksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
 Write-Output "Running BetterDiscord installer"
 Start-Process -FilePath $installLocation -PassThru
 Write-Output "Running Autohotkey script"
--- packages/jarfix/tools/chocolateyinstall.ps1
+++ packages/jarfix/tools/chocolateyinstall.ps1
@@ -1,19 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$location = Join-Path $toolsDir 'jarfix.exe'
 
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'exe'
-  url            = 'https://johann.loefflmann.net/downloads/jarfix.exe'
-  url64bit       = 'https://johann.loefflmann.net/downloads/jarfix.exe'
-  checksum       = '3A00C5B808954E9DCA76418506EACEC9CB1CB0FD844318A896EBAE787F5EAAE2'
-  checksum64     = '3A00C5B808954E9DCA76418506EACEC9CB1CB0FD844318A896EBAE787F5EAAE2'
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  silentArgs     = '/S'
+  packageName  = $packageName
+  fileType     = 'exe'
+  fileFullPath = $location
+  url          = 'https://johann.loefflmann.net/downloads/jarfix.exe'
+  checksum     = '3A00C5B808954E9DCA76418506EACEC9CB1CB0FD844318A896EBAE787F5EAAE2'
+  checksumType = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs
