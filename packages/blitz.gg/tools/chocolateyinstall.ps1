ff20b64aba2e82e463f56f3440663c1137eb7848
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-01-29 15:08:28 +0100

    feat: 🎸 Update blitz.gg to 1.16.21 & Fix automatic update

--- packages/blitz.gg/tools/chocolateyinstall.ps1
+++ packages/blitz.gg/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://blitz-stable.blitz.gg/Blitz-1.16.19.exe'
-  checksum       = ''
+  url            = 'https://blitz-stable.blitz.gg/Blitz-1.16.21.exe'
+  checksum       = '357e2f7e9514afc8767085298065a106af8cde246652fb37b0f769a7645a0203'
   checksumType   = 'sha256'
   silentArgs     = '/S'
 }
