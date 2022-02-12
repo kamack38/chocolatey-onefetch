9410bed2d84d257e00bd1d8016365b3903cd7d92
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-12 22:01:17 +0100

    fix: 🐛 Fix nvm.install

--- packages/nvm.install/tools/chocolateybeforemodify.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$ErrorActionPreference = 'Stop';
-
-$packageName= 'nvm' # arbitrary name for the package, used in messages
-# This next part assumes nvm is on your path currently
-$nvm = (& where.exe $packageName)
-
-& $nvm off
