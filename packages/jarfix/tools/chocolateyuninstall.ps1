f301585e45f7b8240ac4286fd026f4f5f888aeab
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-14 08:41:24 +0100

    fix: 🐛 Jarfix: chnage error action

--- packages/jarfix/tools/chocolateyuninstall.ps1
+++ packages/jarfix/tools/chocolateyuninstall.ps1
@@ -1,3 +1,3 @@
 $shortcutPath = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Jarfix.lnk"
 
-Remove-Item -Path $shortcutPath -Force
+Remove-Item -Path $shortcutPath -Force -ErrorAction SilentlyContinue
