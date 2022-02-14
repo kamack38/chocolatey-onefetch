a1b551f34544c326291a47a1ee23fa2f4ae39cc4
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-14 12:32:16 +0100

    feat: 🎸 Update install & unistall in borderlessminecraft

--- packages/borderlessminecraft/tools/chocolateyinstall.ps1
+++ packages/borderlessminecraft/tools/chocolateyinstall.ps1
@@ -1,18 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-
+$location = Join-Path $toolsDir 'BorderlessMinecraft.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'exe'
-  url           = 'https://github.com/Mr-Technician/BorderlessMinecraft/releases/download/v1.3.2/BorderlessMinecraft.exe'
-  checksum      = '6f8a72539ceebeb5d57c14b96c7b868ee8d1b6a8fc0e7ad2443883b3bafed28d'
-  checksumType  = 'sha256'
-  unzipLocation = $toolsDir
+  packageName  = $packageName
+  url          = 'https://github.com/Mr-Technician/BorderlessMinecraft/releases/download/v1.3.2/BorderlessMinecraft.exe'
+  checksum     = '6f8a72539ceebeb5d57c14b96c7b868ee8d1b6a8fc0e7ad2443883b3bafed28d'
+  checksumType = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs
+$shortcutArgs = @{
+  shortcutFilePath = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\BorderlessMinecraft.lnk"
+  targetPath       = $location
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyShortcut @shortcutArgs
