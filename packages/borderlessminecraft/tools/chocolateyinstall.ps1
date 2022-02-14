84872b0174569d4ccd48a66df59a69af8a7d16cf
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-14 14:15:43 +0100

    fix: 🐛 Package: borderlessminecraft

--- packages/borderlessminecraft/tools/chocolateyinstall.ps1
+++ packages/borderlessminecraft/tools/chocolateyinstall.ps1
@@ -9,6 +9,7 @@ $packageArgs = @{
   url          = 'https://github.com/Mr-Technician/BorderlessMinecraft/releases/download/v1.3.2/BorderlessMinecraft.exe'
   checksum     = '6f8a72539ceebeb5d57c14b96c7b868ee8d1b6a8fc0e7ad2443883b3bafed28d'
   checksumType = 'sha256'
+  fileFullPath = $location
 }
 
 $shortcutArgs = @{
