01d724025d0c0b94bc798f64d8edbd4f5e9402fa
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-15 09:25:21 +0100

    feat: 🎸 Add croc package

---
+++ packages/croc/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = 'https://github.com/schollz/croc/releases/download/v9.5.1/croc_9.5.1_Windows-32bit.zip'
+  url64bit       = 'https://github.com/schollz/croc/releases/download/v9.5.1/croc_9.5.1_Windows-64bit.zip'
+  checksum       = '9914cfe61e350780e8c8368823bf7f8d3556c20354853870d115a445aa372f87'
+  checksum64     = 'b29852a14721c5459f79f7f9942c5d4ba3f44ddf7c30f23ec7d734806ae4e10b'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
--- packages/onefetch/tools/chocolateyinstall.ps1
+++ packages/onefetch/tools/chocolateyinstall.ps1
@@ -7,13 +7,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'exe'
-  url            = 'https://github.com/o2sh/onefetch/releases/download/v2.11.0/onefetch-win.tar.gz'
-  checksum       = '0359dafb5ea5c165f92a1ee7ed8b5f607771aad4666da14b819886c7605d533e'
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsDir
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = 'https://github.com/o2sh/onefetch/releases/download/v2.11.0/onefetch-win.tar.gz'
+  checksum      = '0359dafb5ea5c165f92a1ee7ed8b5f607771aad4666da14b819886c7605d533e'
+  checksumType  = 'sha256'
+  unzipLocation = $toolsDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
