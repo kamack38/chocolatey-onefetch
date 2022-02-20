a8b9b57d73e27623863122a0a0694a8f4e59c404
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-20 19:13:29 +0100

    🔼 Bump blitz.gg to 1.16.29

--- packages/blitz.gg/tools/chocolateyinstall.ps1
+++ packages/blitz.gg/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://blitz-stable.blitz.gg/Blitz-1.16.24.exe'
-  checksum       = 'ce391a2be856e64c0607545d4afcf624fa72776cea18f92a8be776984ef458a8'
+  url            = 'https://blitz-stable.blitz.gg/Blitz-1.16.29.exe'
+  checksum       = '0219811e3de5de088efd7b92a358ad160ffbfbcffe7d966fd0ccc9041ca52957'
   checksumType   = 'sha256'
   silentArgs     = '/S'
 }
