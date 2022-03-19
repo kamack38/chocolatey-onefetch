14f5a2a65f040ec4e7e2aa3542323c5c9f0fd402
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-03-19 14:03:54 +0100

    fix: 🐛 Fix windhawk

--- packages/windhawk/tools/chocolateyinstall.ps1
+++ packages/windhawk/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   url          = 'https://github.com/ramensoftware/windhawk/releases/download/v0.9.1/windhawk_setup.exe'
   checksum     = 'a14587bb13c84415e9e8128937f315c36832d09006ce2e09db7ea632eb19f1fd'
   checksumType = 'sha256'
-  silentArgs   = '/S'
+  silentArgs   = '/STANDARD /S'
 }
 
 Install-ChocolateyPackage @packageArgs
