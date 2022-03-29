85a9c70ac6b6c738cd0b741f4d7d7865b8228b52
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-03-29 22:58:24 +0200

    feat: 🎸 Update `onefetch` from `2.11.0` to `2.12.0`

--- packages/onefetch/tools/chocolateyinstall.ps1
+++ packages/onefetch/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
-  url           = 'https://github.com/o2sh/onefetch/releases/download/v2.11.0/onefetch-win.tar.gz'
-  checksum      = '0359dafb5ea5c165f92a1ee7ed8b5f607771aad4666da14b819886c7605d533e'
+  url           = 'https://github.com/o2sh/onefetch/releases/download/v2.12.0/onefetch-win.tar.gz'
+  checksum      = '76693b66642fefd1cf9729e82b58ee2c67644d91f4e4b0de2d072697e5afa03a'
   checksumType  = 'sha256'
   unzipLocation = $toolsDir
 }
